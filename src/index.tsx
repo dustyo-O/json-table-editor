6bc7f4a6a9cd01652e28e20740fe67c88bfa1531
Alexandr Shleyko <dusty@yandex-team.ru>
2023-08-19 21:46:03 +0400

    wip

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
 import './index.css';
+import 'react-virtualized/styles.css';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
